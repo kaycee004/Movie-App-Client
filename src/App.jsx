fb8d4a4801ece2eace16bfa36c14364efde9cd6f
kaycee04 <kingsleyotuya04@gmail.com>
2023-10-26 15:08:02 +0100

    update signin import

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,31 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Movies from "./pages/Movies/Movies";
 import TvSeries from "./pages/TvSeries/TvSeries";
-import Signin from "./pages/signin/Signin";
-import Signup from "./pages/signup/Signup";
+import Signin from "./pages/SignIn/SignIn";
+import Signup from "./pages/SignUp/SignUp";
 import Bookmark from "./pages/Bookmark/Bookmark";
 import Error from "./pages/Error/Error";
 import RootLayout from "./layouts/RootLayout";
 import AuthLayout from "./layouts/AuthLayout";
 import { AuthProvider } from "./context/AuthContext";
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from "react-hot-toast";
 import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
   return (
     <>
       <Router>
-        <Toaster position='top-right'/>
+        <Toaster position="top-right" />
         <AuthProvider>
           <Routes>
             <Route element={<RootLayout />}>
               <Route path="/" element={<Home />} />
               <Route path="/movies" element={<Movies />} />
               <Route path="/tvseries" element={<TvSeries />} />
-            <Route element={<PrivateRoute/>}>
-              <Route path="/bookmark" element={<Bookmark />} />
+              <Route element={<PrivateRoute />}>
+                <Route path="/bookmark" element={<Bookmark />} />
+              </Route>
             </Route>
-            </Route>
-
 
             <Route element={<AuthLayout />}>
               <Route path="/signin" element={<Signin />} />
